f270f7ad77f2aac5a1db86262a98b6d3bc3a18fc
ammer-ahmad <ameer5ahmad@gmail.com>
2021-05-08 17:52:00 -0400

    added on boarding page and styles

---
+++ src/components/Onboarding.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Onboarding = () => {
+  return (
+    <>
+      <div className="onboarding">
+        <h1>
+          Shoppies<span style={{ color: " #95BF47" }}>.</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac sit
+          tristique nam tortor rhoncus dignissim interdum id. Feugiat lorem
+          adipiscing.
+        </p>
+        <button>Get Started</button>
+      </div>
+      <div></div>
+    </>
+  );
+};
+
+export default Onboarding;
