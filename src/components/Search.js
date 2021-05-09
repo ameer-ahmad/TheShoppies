a55e14b23d08269c6027435d451ee74586a27728
ammer-ahmad <ameer5ahmad@gmail.com>
2021-05-08 21:34:49 -0400

    added styles for search box

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../img/logo.png";
 import searchIcon from "../img/search.png";
 
 const Search = () => {
+  // State for search results
+  const [query, setQuery] = useState("");
+  // State for movies
+  const [movies, setMovies] = useState([]);
+
+  const searchQueries = async (e) => {
+    setQuery(e.target.value);
+
+    const url = `http://www.omdbapi.com/?s=${query}&apikey=92b81fc0`;
+
+    try {
+      const res = await fetch(url);
+      const movies = await res.json();
+      console.log(movies);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div class="searchContainer">
+    <div className="searchContainer">
       <img className="logo" src={logo} alt="logo" />
       <div class="input-wrapper">
         <img src={searchIcon} />
-        <input type="text" placeholder="Search for a Movie..." />
+        <input
+          name="query"
+          type="text"
+          placeholder="Search for a Movie..."
+          value={query}
+          onChange={searchQueries}
+        />
       </div>
     </div>
   );
